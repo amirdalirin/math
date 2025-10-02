a901d9f27f20a93a1d632eda2a79d3b0a69c4595
amirdalirin <amirdaliri1387@gmail.com>
2025-10-02 21:44:52 +0330

    drow a cuibic function

--- f(x).py
+++ f(x).py
@@ -1,13 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(-10,100,100)
+# تعریف تابع درجه سه
+def cubic(a, b, c, d, x):
+    return a*x**3 + b*x**2 + c*x + d
 
-y = np.exp2(x)
+# گرفتن ضرایب از کاربر
+a = float(input("ضریب a (x³): "))
+b = float(input("ضریب b (x²): "))
+c = float(input("ضریب c (x): "))
+d = float(input("ضریب d (ثابت): "))
 
+# بازه x
+x = np.linspace(-10, 10, 500)
+y = cubic(a, b, c, d, x)
 
-
-plt.plot(x, y)
-
-
+# رسم نمودار
+plt.plot(x, y, label=f"f(x) = {a}x³ + {b}x² + {c}x + {d}")
+plt.axhline(0, color='black', linewidth=0.8)
+plt.axvline(0, color='black', linewidth=0.8)
+plt.legend()
+plt.grid(True)
+plt.title("نمودار تابع درجه سه")
 plt.show()
